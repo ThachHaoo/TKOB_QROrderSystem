d47291f30252129b1749755d4625ce19aa690ff2
nphoang <nphuchoang.itus@gmail.com>
2025-11-26 18:13:44 +0700

    feat(auth): implement auth service

---
+++ source/apps/api/src/modules/redis/redis.module.ts
@@ -0,0 +1,42 @@
+import { Global, Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { EnvConfig } from "src/config/env.validation";
+import { RedisService } from './redis.service';
+
+@Global()
+@Module({
+  imports: [ConfigModule],
+  providers: [
+    {
+      provide: 'REDIS_CLIENT',
+      useFactory: async (configService: ConfigService<EnvConfig, true>) => {
+        const { Redis } = await import('ioredis');
+
+        const client = new Redis({
+          host: configService.get('REDIS_HOST', { infer: true }),
+          port: configService.get('REDIS_PORT', { infer: true }),
+          password: configService.get('REDIS_PASSWORD', { infer: true }),
+          db: configService.get('REDIS_DB', { infer: true }),
+          retryStrategy: (times) => {
+            const delay = Math.min(times * 50, 2000);
+            return delay;
+          }
+        });
+
+        client.on('error', (err) => {
+          console.error('Redis Client Error', err);
+        });
+
+        client.on('connect', () => {
+          console.log('Redis Client Connected');
+        });
+        
+        return client;
+      },
+      inject: [ConfigService],
+    },
+    RedisService,
+  ],
+  exports: [RedisService],
+})
+export class RedisModule {}
